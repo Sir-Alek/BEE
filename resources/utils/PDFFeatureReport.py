db185349ee48d162863456d6bcf078e101511b6e
Sir Alek <j_alejandro.ramirez@hotmail.com>
2026-03-17 16:15:12 -0600

    MOD| Update logo path in PDFFeatureReport

--- resources/utils/PDFFeatureReport.py
+++ resources/utils/PDFFeatureReport.py
@@ -16,9 +16,8 @@ class PDFFeatureReport(FPDF):
         self.scenarios = []
     
     def header(self):
-        # Logo
-        self.image(os.path.join('resources', 'resourcesPDF', 'logo-servicios-financieros.jpg'), 
-                  x=70, y=15, w=60, h=15)
+        # Logo de BEE con validación de existencia
+        logo_path = os.path.join('resources', 'logo_bee_png_transparente.png')
         
         # Título con espacio ajustado
         self.set_y(35)
